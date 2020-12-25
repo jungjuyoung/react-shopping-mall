6caded3894edfaa7e080bfa397a84a5510ae9bd7
정주영 <nwwn1004@naver.com>
2020-12-26 01:14:55 +0900

    상품 업로드 완료

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import FileUpload from '../../utils/FileUpload';
+import Axios from 'axios';
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -15,12 +16,11 @@ const Continents = [
   { key: 7, value: 'Antarctica' },
 ];
 
-function UploadProductPage() {
+function UploadProductPage(props) {
   const [TitleValue, setTitleValue] = useState('');
   const [DescriptionValue, setDescriptionValue] = useState('');
   const [PriceValue, setPriceValue] = useState(0);
   const [ContinentValue, setContinentValue] = useState(1);
-
   const [Images, setImages] = useState([]);
 
   const onTitleChange = (e) => {
@@ -40,7 +40,37 @@ function UploadProductPage() {
     setImages(newImages);
   };
 
-  const onSubmit = () => {};
+  const onSubmit = (e) => {
+    console.log('@@@ onSubmit 실행');
+    e.preventDefault();
+    if (
+      !TitleValue ||
+      !DescriptionValue ||
+      !PriceValue ||
+      !ContinentValue ||
+      !Images
+    ) {
+      return alert('모든 값을 채워넣어주세요!');
+    }
+    const body = {
+      // 로그인된 사람의 ID
+      writer: props.user.userData._id,
+      title: TitleValue,
+      description: DescriptionValue,
+      price: PriceValue,
+      dontinents: ContinentValue,
+      images: Images,
+    };
+    // 서버에 채운 값들을 request로 보낸다
+    Axios.post('/api/product', body).then((res) => {
+      if (res.data.success) {
+        alert('상품 업로드에 성공 했습니다.');
+        props.history.push('/');
+      } else {
+        alert('상품 업로드에 실패 했습니다.');
+      }
+    });
+  };
   return (
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -75,7 +105,7 @@ function UploadProductPage() {
         </select>
         <br />
         <br />
-        <Button>Submit</Button>
+        <button type="submit">Submit</button>
       </Form>
     </div>
   );
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const { Product } = require('../models/Product');
 
 const { auth } = require('../middleware/auth');
 
@@ -37,6 +38,17 @@ router.post('/uploadImage', auth, (req, res) => {
       filePath: res.req.file.path,
       fileName: res.req.file.filename,
     });
+  });
+});
+
+router.post('/', (req, res) => {
+  // 받아온 파일 정보들을 DB에 넣어준다.
+  const product = new Product(req.body);
+  product.save((err) => {
+    if (err) {
+      return res.status(400).json({ success: false, err });
+    }
+    return res.status(200).json({ success: true });
   });
 });
 
